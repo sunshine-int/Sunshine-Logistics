3ef079dbd5916b7ab63e3e6c3e216ce35f0855ed
sunshine-int <sunshine0422520071@gmail.com>
2026-06-09 13:31:24 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -215,14 +215,14 @@ function App() {
             <div className="mt-8 flex flex-col gap-4 pb-8 sm:flex-row sm:flex-wrap">
             <Button
   href="#quote"
-  className="min-w-[260px] justify-center rounded-[32px] px-16 py-7 text-3xl font-black"
+  className="min-w-[260px] justify-center rounded-[32px] px-16 py-7 text-2xl font-black"
 >
   線上詢價
 </Button>
 
 <Button
   href="#about"
-  className="min-w-[260px] justify-center rounded-[32px] px-16 py-7 text-3xl font-black"
+  className="min-w-[260px] justify-center rounded-[32px] px-16 py-7 text-2xl font-black"
 >
   關於尚順
 </Button>
